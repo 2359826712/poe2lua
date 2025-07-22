fb32b7aebb1629ec3987e30319d75d303ff281c2
lsx <2359826712@qq.com>
2025-07-22 08:50:48 +0800

    更新主程序逻辑，移除临时UI遍历与文本点击代码，调整可执行文件忽略规则并新增.exe类型忽略项

--- main.lua
+++ main.lua
@@ -8,42 +8,22 @@ print(player_info.grid_y)
 if player_info.grid_x == 0 then
     return
 end
--- otherworld.run()
+otherworld.run()
 -- 运行行为树
 -- print("开始运行行为树...")
 -- while true do
 --       -- 清空节点栈和YIELD标记
 --     bt:run()
 -- end
-UI_info = {}
-local size =  UiElements:Update()
-print(size .. "\n")
-if size > 0 then
-    print(size .. "\n")
-    local sum = 0;
-    for i = 0, size - 1, 1 do
-        sum = sum + 1
-        -- print(sum .. " " .. UiElements[i].name_utf8 .. '' .. UiElements[i].text_utf8)
-        table.insert(UI_info, UiElements[i])
-    end
-end
-while true do
-    text_table = {
-        ['Checkpoints'] = {1013, 25},
-        ['寶石切割'] = {1013, 25},
-        ['世界地圖'] = {1013, 25},
-        ['天賦技能'] = {1013, 25}
-    }
-    -- 遍历查找并点击
-    for text, pos in pairs(text_table) do
-        print(text)
-        local found = poe2_api.find_text({
-            text = text,
-            min_x = min_x
-        })
-        -- 如果找到，更新坐标表
-        if found then
-            api_ClickScreen(pos[1], pos[2], 1)
-        end
-    end
-end
+-- UI_info = {}
+-- local size =  UiElements:Update()
+-- print(size .. "\n")
+-- if size > 0 then
+--     print(size .. "\n")
+--     local sum = 0;
+--     for i = 0, size - 1, 1 do
+--         sum = sum + 1
+--         -- print(sum .. " " .. UiElements[i].name_utf8 .. '' .. UiElements[i].text_utf8)
+--         table.insert(UI_info, UiElements[i])
+--     end
+-- end
